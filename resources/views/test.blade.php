02642c85051843b8a80092285f1749aed99200ce
BilalKhatib <khatib961@outlook.com>
2023-02-06 01:19:34 +0200

    done setup + new middleware + new admin guard

---
+++ resources/views/test.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.admin')
+
+@section('title')
+Dashboard
+@endsection
+
+@section('contentHeader')
+Section Header
+@endsection
+
+@section('contentHeaderLink')
+<a href="#">Section Link</a>
+@endsection
+
+@section('contentHeaderActive')
+Active Section
+@endsection
+
+@section('content')
+test view
+@endsection
